7f77d36240010bcd926004e6d72861eb629be498
Vladimir <vova2066@yandex.ru>
2024-03-02 10:43:34 +0300

    add scroll text about

---
+++ src/blocks/about/about.js
@@ -0,0 +1,27 @@
+const about = document.getElementById("about");
+
+if (about) {
+
+  const aboutTextList = about.querySelectorAll(".about__text");
+
+  aboutTextList.forEach(scrollText);
+
+  function scrollText (aboutTextBlock) {
+    const options = {
+      threshold: 1,
+      rootMargin: '100px 0px -40% 0px',
+    };
+
+    const pList = aboutTextBlock.querySelectorAll("p");
+
+    function handlerScrollText (entries) {
+      entries.forEach(entry => {
+        entry.target.classList.toggle('scroll-text', entry.isIntersecting);
+      });
+    };
+    const observer = new IntersectionObserver(handlerScrollText, options)
+    pList.forEach(p => observer.observe(p));
+  }
+
+}
+
