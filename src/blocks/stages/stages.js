4e5c1f97985976c05f3164f6bbd7f85d4fb6d006
Vladimir <vova2066@yandex.ru>
2024-03-03 10:26:58 +0300

    ref stages

--- src/blocks/stages/stages.js
+++ src/blocks/stages/stages.js
@@ -3,24 +3,37 @@ const stages = document.getElementById("stages");
 if (stages) {
 
   const stagesList = stages.querySelectorAll(".stages__item");
+  const stagesImages = stages.querySelectorAll(".stages__image-wrapper img");
 
   stagesList.forEach(scrollStages);
 
-  function scrollStages (stagesItem) {
+  function scrollStages (stageItem) {
 
     const options = {
       threshold: 1,
-      rootMargin: '100px 0px -50% 0px',
+      rootMargin: '-30% 0px -35% 0px',
     };
 
-
-    function handlerScrollText (entries) {
-      entries.forEach(entry => {
+    function handlerScrollStages (entries) {
+      entries.forEach((entry) => {
         entry.target.classList.toggle('js-stages-scroll', entry.isIntersecting);
+
+        const dataStage = +stageItem.dataset.stage;
+        const stageIndex = dataStage - 1;
+
+        if (stageItem.classList.contains('js-stages-scroll')) {
+          stagesImages.forEach(img => img.classList.remove('js-stages-scroll'));
+          stagesImages[stageIndex].classList.remove('js-abroad');
+          stagesImages[stageIndex].classList.add('js-stages-scroll');
+        } else {
+          stagesImages[stageIndex].classList.add('js-abroad');
+        }
+
       });
     };
-    const observer = new IntersectionObserver(handlerScrollText, options)
-    observer.observe(stagesItem)
+
+    const observer = new IntersectionObserver(handlerScrollStages, options)
+    observer.observe(stageItem);
   }
 
 }
