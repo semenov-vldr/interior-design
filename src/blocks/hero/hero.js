0b40640d9d8233933743896c141713110877828a
Vladimir <vova2066@yandex.ru>
2024-03-07 01:36:27 +0300

    start portfolio

--- src/blocks/hero/hero.js
+++ src/blocks/hero/hero.js
@@ -1,3 +1,7 @@
+const hero = document.querySelector(".hero");
+
+if (hero) {
+
 const swiper = new Swiper('.hero__slider', {
 
   // Optional parameters
@@ -23,7 +27,29 @@ const swiper = new Swiper('.hero__slider', {
   scrollbar: {
     el: document.getElementById('swiperScrollbar'),
   },
-
-
-
 });
+
+
+  const heroSlides = hero.querySelectorAll('.hero__slider .swiper-slide');
+  const swiperScrollbar = document.getElementById('swiperScrollbar');
+  const heroLink = hero.querySelector('.hero__link');
+
+    function updateScrollbarClass () {
+    const activeSlideIndex = Array.from(heroSlides)
+      .findIndex(slide => slide.classList.contains('swiper-slide-active'));
+    swiperScrollbar.classList.remove('first', 'second', 'third');
+
+    if (activeSlideIndex !== -1) {
+      const classNames = ['first', 'second', 'third'];
+      swiperScrollbar.classList.add(classNames[activeSlideIndex]);
+
+      const activeSlideLink = heroSlides[activeSlideIndex].dataset.src;
+      heroLink.href = activeSlideLink;
+    }
+  };
+
+  let observerHero = new MutationObserver(updateScrollbarClass);
+  heroSlides.forEach(slide => observerHero.observe(slide, {attributes: "class"}));
+
+
+}
