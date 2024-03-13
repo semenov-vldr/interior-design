8a345f62f44be930a3f3efae4edbcc6cbbbe0e26
semenov-vldr <semenov-vldr@users.noreply.github.com>
2024-03-13 22:43:12 +0000

    deploy: 30f21f8530d043b10837f59b529f9e515bde00cd

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -131,27 +131,25 @@ if (about) {
 var text = "Более 6 лет создаем интерьер передающий черты его владельца, учитывая образ жизни, увлечения и пожелания всех членов семьи, красивая и удобная обстановка в доме положительно сказывается на настроении, здоровье и финансовом успехе";
 var about = document.getElementById("about");
 if (about) {
-  var scrollingText = function scrollingText() {
-    var scrollPercentage = window.scrollY / (document.body.offsetHeight - window.innerHeight);
-    var spans = document.querySelectorAll('#paragraph span');
-    var numSpansToChange = Math.ceil(scrollPercentage * 4 * spans.length);
-    spans.forEach(function (span) {
-      span.style.opacity = 0.2;
-    });
-    for (var i = 0; i < numSpansToChange; i++) {
-      spans[i].style.opacity = 1;
-    }
-  };
+  gsap.registerPlugin(ScrollTrigger);
   var parentElement = document.getElementById('paragraph');
   var words = text.split(' ');
   words.forEach(function (word) {
     var span = document.createElement('span');
-    span.style.opacity = 0.2;
+    // span.style.opacity = 0.2;
     span.textContent = word + ' ';
     parentElement.appendChild(span);
   });
-  ;
-  window.addEventListener('scroll', scrollingText);
+  gsap.to("#paragraph span", {
+    backgroundPositionX: "0%",
+    stagger: 1,
+    scrollTrigger: {
+      trigger: "#paragraph",
+      scrub: true,
+      start: "top 75%",
+      end: "top 30%"
+    }
+  });
 }
 "use strict";
 
