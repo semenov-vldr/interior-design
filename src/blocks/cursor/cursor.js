3ba11f14fa3a22abfbbe512a6a356fe6ef4d47ff
Vladimir <vova2066@yandex.ru>
2024-03-20 23:30:53 +0300

    add cursor

---
+++ src/blocks/cursor/cursor.js
@@ -0,0 +1,34 @@
+const cursor = document.querySelector('.cursor');
+
+if (cursor) {
+  const $bigBall = document.querySelector('.cursor__ball--big');
+  const $smallBall = document.querySelector('.cursor__ball--small');
+
+  document.body.addEventListener('mousemove', onMouseMove);
+
+
+  function onMouseMove(e) {
+    gsap.to($bigBall, .4, {
+      x: e.pageX - 12,
+      y: e.pageY - 12
+    });
+
+    gsap.to($smallBall, .1, {
+      x: e.pageX - 5,
+      y: e.pageY - 8
+    });
+
+
+    const footer = document.querySelector("#footer");
+
+    if (footer && e.pageY > document.documentElement.scrollHeight - footer.offsetHeight) {
+      cursor.classList.add("hover-footer");
+    } else {
+      cursor.classList.remove("hover-footer");
+    }
+
+  }
+
+}
+
+
