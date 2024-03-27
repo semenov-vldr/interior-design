35f1a4818b93157bdb01cf9347821465f2072f0d
Vladimir <vova2066@yandex.ru>
2024-03-27 22:30:18 +0300

    add alert email, updated tel, email

--- src/blocks/footer/footer.js
+++ src/blocks/footer/footer.js
@@ -4,6 +4,13 @@ function margin() {
   const footer = document.querySelector(".footer");
   if (footer) {
     document.body.style.marginBottom = footer.offsetHeight + "px";
+
+    // Copy email
+    const email = footer.querySelector(".footer__contacts--email");
+    email.addEventListener("click", function() {
+      window.navigator.clipboard.writeText(this.textContent);
+      alert("Почта скопирована в буфер обмена");
+    });
   }
 }
 
